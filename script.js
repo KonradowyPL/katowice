a6156338cd954bd830eb033dc38a74b84971ed89
KonradowyPL <konrad.chrzaszczak@gmail.com>
2024-02-19 18:11:23 +0100

    fix routes, unlock all, meta

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function loadLocked() {
 
 function loadRoutes() {
   var ele = "";
-  var badges = []
+  var badges = [];
   Object.keys(routes).forEach((key, i) => {
     const route = routes[key];
     var thisele = "";
@@ -275,33 +275,33 @@ function loadRoutes() {
       }
     });
     if (route.destonations.length == completed) {
-      badges.push(key)
+      badges.push(key);
     }
 
     ele +=
-      `<div class="route visited nonVisited"><div class="routeTitle"><span>${route.name}</span><span>${completed}/${route.destonations.length}</span></div><div class="routeDiscreption">${route.discreption}</div><ul>` +
+      `<div class="route visited nonVisited"><div class="routeMeta"><img src="${key}.svg"><div class="routeTitle"><span>${route.name}</span><span>${completed}/${route.destonations.length}</span></div><span class="routeDiscreption">${route.discreption}</span></div><ul>` +
       thisele +
       "</ul></div>";
   });
   routesObj.innerHTML = ele;
-  const sel = document.querySelector("#badges .selected")?.src
-  document.getElementById("badges").innerHTML = ""
+  const sel = document.querySelector("#badges .selected")?.src;
+  document.getElementById("badges").innerHTML = "";
   badges.forEach((e) => {
-    const ele = document.createElement("img")
-    ele.src = e + ".svg"
+    const ele = document.createElement("img");
+    ele.src = e + ".svg";
     if (sel == ele.src) {
-      ele.classList.add("selected")
+      ele.classList.add("selected");
     }
 
     ele.onclick = (event) => {
-      Array.from(document.getElementById("badges").children).forEach(e => e.classList.remove("selected"))
-      ele.classList.add("selected")
+      Array.from(document.getElementById("badges").children).forEach((e) => e.classList.remove("selected"));
+      ele.classList.add("selected");
 
-      badgeInfo.innerHTML = `Odznaka za odwiedzenie wszystkich miejsc na szlaku <b>${routes[e]?.nn || routes[e]?.name}</b>.`
-    }
+      badgeInfo.innerHTML = `Odznaka za odwiedzenie wszystkich <b>${routes[e]?.nn || routes[e]?.name}</b>.`;
+    };
 
-    document.getElementById("badges").appendChild(ele)
-  })
+    document.getElementById("badges").appendChild(ele);
+  });
 }
 
 u(window.location.hash);
@@ -363,3 +363,7 @@ navigator.permissions.query({ name: "geolocation" }).then((permissionStatus) => 
     localisationUpdateInterval = navigator.geolocation.watchPosition(updateUserPos, localisationError);
   };
 });
+
+if (new URLSearchParams(window.location.search).get("unlockAll") == "true") {
+  unlockAll();
+}
