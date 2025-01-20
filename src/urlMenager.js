1ee19c1cf1440975f61a29d4b6da518931f2aeb4
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2025-01-20 17:35:14 +0100

    refacter map

--- src/urlMenager.js
+++ src/urlMenager.js
@@ -4,36 +4,49 @@ var places = [];
 var displayPlace = undefined;
 
 const u = (url) => {
-  const map = new Map();
-  map.set(/^https:\/\/.+$/, () => {
-    window.open(url, "_blank").focus();
-  });
-  map.set(/^#mainmenu.*$/, () => {
-    mainmenu.classList.remove("hidden");
-    backButton.href = "#map";
-  });
-  map.set(/^#map$/, () => {
-    backButton.href = "#mainmenu";
-    mainmenu.classList.add("hidden");
-  });
-  map.set(/^#map:(\d+)(&?)$/, ()=> {
-    const match = url.match(/^#map:(\d+)(&?)$/)
-    console.log("called", match)
-    const id = match[1]|0;
-    const move = !!match[2] 
-    const place = places.find((place) => place.id == id);
-    console.log(id,move,place)
-    if (place) {
-      displayPlace(id, move);
-    } else {
-      tooltips.style.height = "0%";
-    }
-  })
+  const map = new Map([
+    [
+      /^https:\/\/.+$/,
+      () => {
+        window.open(url, "_blank").focus();
+      },
+    ],
+    [
+      /^#mainmenu.*$/,
+      () => {
+        mainmenu.classList.remove("hidden");
+        backButton.href = "#map";
+      },
+    ],
+    [
+      /^#map$/,
+      () => {
+        backButton.href = "#mainmenu";
+        mainmenu.classList.add("hidden");
+      },
+    ],
+    [
+      /^#map:(\d+)(&?)$/,
+      () => {
+        const match = url.match(/^#map:(\d+)(&?)$/);
+        console.log("called", match);
+        const id = match[1] | 0;
+        const move = !!match[2];
+        const place = places.find((place) => place.id == id);
+        console.log(id, move, place);
+        if (place) {
+          displayPlace(id, move);
+        } else {
+          tooltips.style.height = "0%";
+        }
+      },
+    ],
+  ]);
 
   for (const [regex, handler] of map) {
     if (regex.test(url)) {
-      console.log(regex, handler, handler.toString())
-      return handler()
+      console.log(regex, handler, handler.toString());
+      return handler();
     }
   }
 };
