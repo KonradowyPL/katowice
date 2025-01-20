ee85b2077dc77c4bd73e2d5f5c4eabcc960366d5
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2025-01-20 21:08:15 +0100

    preload

--- src/script.js
+++ src/script.js
@@ -1,53 +1,5 @@
-sendor()
-
-import { currentPlace, currentPlaceDat } from "./placeinfo.js";
-import { places } from "./loader.js";
-import { u } from "./urlMenager.js";
-import { map } from "./map.js";
-import "./mainmenu.js";
-import "./marker.js";
-import "./swipe.js";
-
-// load current place
-u(window.location.hash);
-if (currentPlace) map.setView(new L.LatLng(currentPlaceDat.lat, currentPlaceDat.lon), 19);
-
-// rerender map when it's size changes
-new ResizeObserver((entries) => entries.forEach(() => map.invalidateSize())).observe(document.getElementById("map"));
-
-
-
-// unlock all easter egg
-const unlockAll = function () {
-  let unlocked = Object.keys(places);
-  localStorage.setItem("unlocked", JSON.stringify(unlocked));
-  let newUrl = new URL(window.location.href);
-  newUrl.searchParams.delete("unlockAll");
-  window.location = newUrl;
-};
-
-var counter = 0;
-dev.onclick = (e) => {
-  counter++;
-
-  if (counter > 6) {
-    unlockAll();
-  }
-
-  setTimeout(() => counter--, 5000);
-};
-
-if (new URLSearchParams(window.location.search).get("unlockAll") == "true") {
-  unlockAll();
-  crash() // bruh
-}
-
-// hide loading screen
-if (document.readyState !== "complete") {
-  window.addEventListener("load", function () {
-    console.info("Loading completed!");
-    document.getElementById("loading").classList.add("fadeout");
-  });
-} else {
-  document.getElementById("loading").classList.add("fadeout");
-}
+import("./main.js").catch(
+  () =>
+    (document.getElementById("loadingStatus").innerText =
+      "Nie udało się załadować aplikacji. Spróbuj ponownie później lub skontaktuj się z administratorem strony.")
+);
